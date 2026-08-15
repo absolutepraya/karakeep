5d779d957303da9ba7529a5201ad81fe54970ccc
Daffa Abhipraya <daffaabhiprayaputra@gmail.com>
2026-08-15 17:30:06 +0700

    fix: support older TypeScript targets in invitation email

--- packages/trpc/email.ts
+++ packages/trpc/email.ts
@@ -50,11 +50,11 @@ function withTracing<Args extends unknown[]>(
 
 function escapeHtml(value: string) {
   return value
-    .replaceAll("&", "&amp;")
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;")
-    .replaceAll('"', "&quot;")
-    .replaceAll("'", "&#39;");
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;");
 }
 
 function sanitizeHeaderValue(value: string) {
